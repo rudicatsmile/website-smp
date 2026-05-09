ffa52756a3709ca47dc8e727db3697d0d7795be1
rudi.catsmile <rudi.catsmile2@gmail.com>
2026-05-09 22:55:59 +0700

    Feat(user):Add counselor on canAccessPanel()

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->hasAnyRole(['super_admin', 'admin', 'editor', 'contributor', 'teacher']);
+        return $this->hasAnyRole(['super_admin', 'admin', 'editor', 'contributor', 'teacher', 'counselor']);
     }
 
     public function staffMember(): HasOne
